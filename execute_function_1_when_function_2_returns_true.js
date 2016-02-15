b0f0fa785718b22ae5a82c43db3dacb5751d4ecf
Cliff Simmons <cliff@cliffsimmons.com>
2016-02-15 15:34:35 -0800

    Create execute_function_1_when_function_2_returns_true.js

---
+++ execute_function_1_when_function_2_returns_true.js
@@ -0,0 +1,21 @@
+function execute_function_1_when_function_2_returns_true( first_function_parementer, second_function_parementer, check_frequency_parameter ){
+
+	if( typeof check_frequency_parameter == 'undefined' ) var check_frequency_parameter = 100;
+
+	if( second_function_parementer() == true ) {
+		
+		first_function_parementer();
+		
+	}
+	
+	else {
+		
+		setTimeout( function(){
+			           				
+			execute_function_1_when_function_2_returns_true( first_function_parementer, second_function_parementer, check_frequency_parameter );
+			
+		}, check_frequency_parameter );
+		
+	}
+
+}
