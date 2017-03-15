3e7858e544afb6821d01d992f9d52d16240b3d39
Chris Landry <topher.p.landry@gmail.com>
2017-03-15 17:41:01 -0400

    Missed a closing brace. Coding is hard.

--- execute_function_1_when_function_2_returns_true.js
+++ execute_function_1_when_function_2_returns_true.js
@@ -1,6 +1,7 @@
 function execute_function_1_when_function_2_returns_true (functionOne, functionTwo) {
   if (functionTwo) {
     functionOne();
+  }
   else {
     setTimeout(function () {
       execute_function_1_when_function_2_returns_true(functionOne, functionTwo);
