83bb0da1cf84a9d883a1e2ef94285dc39a9cd35d
Chris Landry <topher.p.landry@gmail.com>
2017-03-15 17:35:54 -0400

    Code / format cleanup. Also no need to define frequency when it is always 100.

--- execute_function_1_when_function_2_returns_true.js
+++ execute_function_1_when_function_2_returns_true.js
@@ -1,25 +1,9 @@
-<script type="text/javascript">
-
-	function execute_function_1_when_function_2_returns_true( first_function_parementer, second_function_parementer, check_frequency_parameter ){
-
-		if( typeof check_frequency_parameter == 'undefined' ) var check_frequency_parameter = 100;
-	
-		if( second_function_parementer() == true ) {
-			
-			first_function_parementer();
-			
-		}
-		
-		else {
-			
-			setTimeout( function(){
-				           				
-				execute_function_1_when_function_2_returns_true( first_function_parementer, second_function_parementer, check_frequency_parameter );
-				
-			}, check_frequency_parameter );
-			
-		}
-
-	}
-	
-</script>
+function execute_function_1_when_function_2_returns_true (functionOne, functionTwo) {
+  if (functionTwo) {
+    functionOne();
+  else {
+    setTimeout(function () {
+      execute_function_1_when_function_2_returns_true(functionOne, functionTwo);
+    }, 100);
+  }
+}
